83adc3970b20ab3a5c4f48d9231c7343f72ffe70
mateuszluczkowski <mateuszluczkowski@gmail.com>
2022-01-27 13:56:31 +0100

    added interval which changing profession text

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import {
    Background,
    Name,
@@ -12,13 +14,33 @@ import {
 
 import portraitWebp from "assets/img/cs.webp";
 
+const professtionText = [
+   "Front End Developer",
+   "ReactJS",
+   "NodeJS",
+   "MongoDB",
+   "TypeScript",
+];
+
 export default function Header() {
+   const [currentIndex, setCurrentIndex] = useState<number>(0);
+   const text: string = professtionText[currentIndex];
+   const switchProfessionText = () =>
+      setCurrentIndex((prevIndex) => {
+         const nextIndex = prevIndex + 1;
+         return nextIndex >= professtionText.length ? 0 : nextIndex;
+      });
+   console.log(currentIndex);
+   useEffect(() => {
+      const switchInterval = setInterval(() => switchProfessionText(), 10000);
+      return () => clearInterval(switchInterval);
+   });
    return (
       <Root>
          <TextWrapper>
             <SubText children="Hello, My name is" />
             <Name children="Mateusz Łuczkowski" />
-            <Profession children="Front End Developer" />
+            <Profession children={text} />
             <Text children="I design and develop services for customers of all sizes, specializing in creating stylish, modern websites, web services and online stores." />
          </TextWrapper>
          <PhotoWrapper>
